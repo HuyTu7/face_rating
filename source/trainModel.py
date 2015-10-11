70994bc857e3cdb0836e101346f5521dd82d88b5
Avi Singh <avisingh599@gmail.com>
2015-10-11 14:38:57 +0530

    option to train multiple models

--- source/trainModel.py
+++ source/trainModel.py
@@ -1,32 +1,60 @@
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn import linear_model
-from sklearn import decomposition
 
-features = np.loadtxt('features_ALL.txt', delimiter=',')
+from sklearn import decomposition
+from sklearn import linear_model
+from sklearn.ensemble import RandomForestRegressor
+from sklearn import svm
+from sklearn import gaussian_process
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-model', type=str, default='linear_model')
+parser.add_argument('-featuredim', type=str, default=20)
+parser.add_argument('-inputfeatures', type=str, default='../data/features_ALL.txt')
+parser.add_argument('-labels', type=str, default='../data/ratings.txt')
+args = parser.parse_args()
+
+
+features = np.loadtxt(args.inputfeatures, delimiter=',')
 #features = preprocessing.scale(features)
 features_train = features[0:-50]
 features_test = features[-50:]
 
-pca = decomposition.PCA(n_components=20)
+pca = decomposition.PCA(n_components=args.featuredim)
 pca.fit(features_train)
 features_train = pca.transform(features_train)
 features_test = pca.transform(features_test)
 
-ratings = np.loadtxt('labels.txt', delimiter=',')
+ratings = np.loadtxt(args.labels, delimiter=',')
 #ratings = preprocessing.scale(ratings)
 ratings_train = ratings[0:-50]
 ratings_test = ratings[-50:]
 
-regr = linear_model.LinearRegression()
+if args.model == 'linear_model':
+	regr = linear_model.LinearRegression()
+elif args.model == 'svm':
+	regr = svm.SVR()
+elif args.model == 'rf':
+	regr = RandomForestRegressor(n_estimators=50, max_depth=None, min_samples_split=1, random_state=0)
+elif args.model == 'gpr':
+	regr = gaussian_process.GaussianProcess(theta0=1e-2, thetaL=1e-4, thetaU=1e-1)
+else:
+	raise NameError('Unknown machine learning model. Please us one of: rf, svm, linear_model, gpr')
+
 regr.fit(features_train, ratings_train)
 ratings_predict = regr.predict(features_test)
 corr = np.corrcoef(ratings_predict, ratings_test)[0, 1]
-print corr
+print 'Correlation:', corr
 
 residue = np.mean((ratings_predict - ratings_test) ** 2)
-print residue
+print 'Residue:', residue
 
 rangeArray = np.arange(1, 51)
-plt.plot(rangeArray, ratings_test, 'r', rangeArray, ratings_predict, 'b')
+truth, = plt.plot(ratings_test, 'r')
+# Put a white cross over some of the data.
+prediction, = plt.plot(ratings_predict, 'b')
+plt.legend([truth, prediction], ["Ground Truth", "Prediction"])
+
+#plt.plot(rangeArray, ratings_test, 'r', label='ground truth', rangeArray, ratings_predict, 'b', label='ground truth')
 plt.show()
