ffdd22513ee991bd313ba95c32bd91175262408e
Ken Tu <huyqtu7@gmail.com>
2017-12-05 14:36:16 -0500

    Update build_model.py

--- source/deep_learning/build_model.py
+++ source/deep_learning/build_model.py
@@ -18,3 +18,37 @@ import Queue
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',\
                         level=logging.INFO)
+
+class Dataset:
+    def __init__(self, csv_path=None, dump=False):
+        if not dump:
+            self.raw_data = raw_data = pd.read_csv(csv_path) 
+            raw_data.info()
+            queue = Queue.Queue()
+            for index, row in raw_data.iterrows():
+                t = threading.Thread(target=utils.load_image,\
+                args=(row['Attractiveness label'],row['Files'], queue,))
+                t.daemon = True
+                t.start()
+            queue.join()
+            item = queue.get()
+            self.X = item['data']
+            self.Y = item['y']
+            while not queue.empty():
+                item = queue.get()
+                self.X = np.vstack((self.X, item['data']))
+                self.Y = np.vstack((self.Y, item['y']))
+           
+            self.Y /= 5.0
+            self.X.dump('./dataset/data_x.numpy')
+            self.Y.dump('./dataset/data_y.numpy')
+        else:
+            self.X = np.load('./dataset/data_x.numpy')
+            self.Y = np.load('./dataset/data_y.numpy')
+        logging.info("shape of train data: %s"%str(self.X.shape)) 
+        logging.info("Load data Done!")
+    def getTrainTest(self):
+        X_train, X_test, y_train, y_test = train_test_split(self.X, self.Y, \
+                test_size=0.2, random_state=42)
+        return X_train, y_train, X_test, y_test
+      
